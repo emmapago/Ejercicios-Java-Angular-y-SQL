103badbc76d980d523a2326af9f0d2e020c476c9
Emma <emmapago97@gmail.com>
2021-03-03 22:15:37 +0100

    geometria

---
+++ geometria/src/main/java/geometria/main.java
@@ -0,0 +1,26 @@
+package geometria;
+
+import JUnit.Junit09_Geometria.dto.geometria;
+
+public class main {
+
+	public static void main(String[] args) {
+		// Creacion de Objeto Circulo
+		geometria gm = new geometria(2);
+		double areaCirc = gm.areaCirculo(2);
+		gm.setArea(areaCirc);
+		System.out.println(gm);
+	}
+
+		geometria gm = new geometria(2);
+		double areaCirc = gm.areaCuadrado(2);
+		gm.setArea(areaCuadrado);
+		System.out.println(gm);
+}
+
+
+		geometria gm = new geometria(2);
+		double areaTriangulo = gm.areaTriangulo(2);
+		gm.setArea(areaTriangulo);
+		System.out.println(gm);
+}}
---
+++ geometria/src/test/java/geometria/geometriaTest.java
@@ -0,0 +1,106 @@
+package geometria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class geometriaTest {
+
+	public void testcomprobarGeometrica() {
+		double areaCirc = g.areaCirculo(3);
+	}
+
+	
+	@Test
+	public void testGeometriaInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGeometria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreacuadrado() {
+		int actual = gm.areacuadrado(3);
+		int expected = 9;
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void testAreaCirculo() {
+		double actual = g.areaCirculo(3);
+		double expected = 3.1416 * Math.pow(3, 2);
+	
+	@Test
+	public void testAreatriangulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArearectangulo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreapentagono() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArearombo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArearomboide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAreatrapecio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFigura() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
