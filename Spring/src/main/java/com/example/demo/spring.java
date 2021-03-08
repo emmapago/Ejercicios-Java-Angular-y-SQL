5b154df02612ed998d3e8fc59bb49cc1e432dad8
emmapago <78428230+emmapago@users.noreply.github.com>
2021-03-08 21:00:53 +0100

    Add files via upload

---
+++ Spring/src/main/java/com/example/demo/spring.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class spring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Unidad 23 - SPRING Basics");
+
+		System.out.println("Crear un proyecto básico Spring utilizando Maven");
+	}
+
+}
